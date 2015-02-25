975ec750aaf8fcb21233a65ec78f7e3c552a3d52
meiraka <meiraka.pre@gmail.com>
2015-02-26 00:10:33 +0900

    add decode.thread_dat test

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -25,3 +25,15 @@ def test_board_subject():
     ret = list(decode.board_subject(body))
     assert (u'100', u'スレッド1', 2) == ret[0]
     assert (u'200', u'スレッド2', 1000) == ret[1]
+
+
+def test_thread_dat():
+    """Parse thread dat."""
+    body = u"""
+名無し<><>1970/01/01(木) 00:00:00.00 ID:ZZZZZZZZ<>hoge<br>fuga <>スレッド1
+名無し<>sage<>2015/02/23(月) 00:00:00.00 ID:FFFFFFFF<>テスト<>"""
+    ret = list(decode.thread_dat(body))
+    assert (u'名無し', u'', u'1970/01/01(木) 00:00:00.00 ID:ZZZZZZZZ',
+            u'hoge<br>fuga ') == ret[0]
+    assert (u'名無し', u'sage', u'2015/02/23(月) 00:00:00.00 ID:FFFFFFFF',
+            u'テスト') == ret[1]
